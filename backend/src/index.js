9642bcc36945087a07ca422fbe1145c898d142a3
bergony <bergony@hotmail.com>
2020-03-24 18:40:22 -0300

    Back-end Aplicações

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+/**
+ *  Rota / recurso
+ */
+
+ /**
+  * Métodos HTTP
+  *
+  * GET: buscar/listar uma informação no back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informaçõa no Back-end
+  * DELETE: deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parametros
+   *
+   * Query params: Parâmentros nomeados enviado na rota após "?" ( filtro, paginação )
+   * Route params: Parãmentros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySQL, SQlite, PostgreSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoBD, CouchDB, etc
+    */
+
+    /**
+     * Driver SELECT * FROM users
+     * query builder: table('users').select('*').where()
+     */
+
+const express = require('express');
+const cors = require('cors')
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
